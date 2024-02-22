4b70fe4e6adb4bc5d612a231317b9fa19117e779
Kai Hatje <kai.hatje@app-akademie.com>
2024-02-22 10:33:36 +0100

    More comments and commented code for examples on how to use.

--- lib/data/firebase_todo_repository.dart
+++ lib/data/firebase_todo_repository.dart
@@ -11,6 +11,8 @@ import 'package:the_todo_app/domain/todo.dart';
 //   (topic: "Lego bauen", isDone: false),
 // ];
 
+/// Todos um den Inhalt von Firestore zu ersetzen.
+/// Die IDs werden auch von Firestore gesetzt und sind hier nur Platzhalter.
 final List<Todo> _defaultTodos = [
   Todo(id: "UNUSED", topic: "Frühstücken", isDone: true),
   Todo(id: "UNUSED", topic: "Clojure lernen"),
@@ -22,11 +24,95 @@ final List<Todo> _defaultTodos = [
 class FirebaseTodoRepository implements TodoRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  // @override
+  // Stream<List<Todo>> get todos =>
+  //     _firestore.collection('todos').snapshots().map((snapshot) => snapshot.docs
+  //         .map((document) => Todo.fromFirestore(document))
+  //         .toList());
+
   @override
-  Stream<List<Todo>> get todos => _firestore
+  Stream<List<Todo>> get todos {
+    final todoCollectionRef = _firestore.collection('todos');
+    final todosSnapshot = todoCollectionRef.snapshots();
+    final todosStream = todosSnapshot.map(
+        (snapshot) => snapshot.docs.map((e) => Todo.fromFirestore(e)).toList());
+
+    return todosStream;
+  }
+
+  // @override
+  // Stream<List<Todo>> get todos {
+  //   final todoCollectionRef = _firestore.collection('todos');
+  //   final todosSnapshotStream = todoCollectionRef.snapshots();
+  //   return todosSnapshotStream.map((snapshot) {
+  //     final todoDocuments = snapshot.docs;
+  //     final List<Todo> retrievedTodos = [];
+  //     for (final todoDocument in todoDocuments) {
+  //       retrievedTodos.add(Todo.fromFirestore(todoDocument));
+  //     }
+
+  //     return retrievedTodos;
+  //   });
+  // }
+
+  Stream<List<Todo>> get openTodos => _firestore
       .collection('todos')
+      .where("isDone", isEqualTo: false)
       .snapshots()
-      .map((s) => s.docs.map((e) => Todo.fromFirestore(e)).toList());
+      .map((snapshot) =>
+          snapshot.docs.map((doc) => Todo.fromFirestore(doc)).toList());
+
+  Stream<List<Todo>> get completedTodos {
+    final todoCollectionRef = _firestore.collection('todos');
+    final openTodoSnapshots =
+        todoCollectionRef.where("isDone", isEqualTo: true).snapshots();
+    final openTodoStream = openTodoSnapshots.map(
+        (snapshot) => snapshot.docs.map((e) => Todo.fromFirestore(e)).toList());
+
+    return openTodoStream;
+  }
+
+  // Stream<List<Todo>> get completedTodos {
+  //   final CollectionReference<Map<String, dynamic>> todoCollectionRef =
+  //       _firestore.collection('todos');
+  //   final Stream<QuerySnapshot<Map<String, dynamic>>> openTodoSnapshots =
+  //       todoCollectionRef.where("isDone", isEqualTo: true).snapshots();
+  //   final Stream<List<Todo>> openTodoStream = openTodoSnapshots.map(
+  //       (snapshot) => snapshot.docs.map((e) => Todo.fromFirestore(e)).toList());
+
+  //   return openTodoStream;
+  // }
+
+  Future<List<Todo>> getThreeTodos() =>
+      _firestore.collection('todos').limit(3).get().then((snapshot) =>
+          snapshot.docs.map((doc) => Todo.fromFirestore(doc)).toList());
+
+  /// Computed property um die Todos einmalig zu holen.
+  Future<List<Todo>> get todosOnce => _firestore.collection('todos').get().then(
+      (snapshot) => snapshot.docs.map((e) => Todo.fromFirestore(e)).toList());
+
+  /// Funktion um die Todos einmal zu holen.
+  Future<List<Todo>> getTodosOnce() async {
+    final snapshot = await _firestore.collection('todos').get();
+    final todos = snapshot.docs.map((e) => Todo.fromFirestore(e)).toList();
+
+    return todos;
+  }
+
+  // @override
+  // Stream<List<Todo>> get todos {
+  //   final todoCollectionRef = _firestore.collection('todos');
+  //   final todosSnapshotStream = todoCollectionRef.snapshots();
+  //   return todosSnapshotStream.map((snapshot) {
+  //     final todoDocuments = snapshot.docs;
+  //     final List<Todo> retrievedTodos = [];
+  //     for (final todoDocument in todoDocuments) {
+  //       retrievedTodos.add(Todo.fromFirestore(todoDocument));
+  //     }
+
+  //     return retrievedTodos;
+  //   });
+  // }
 
   // @override
   // Stream<List<Todo>> get todos {
@@ -34,23 +120,36 @@ class FirebaseTodoRepository implements TodoRepository {
   //       _firestore.collection('todos');
   //   final Stream<QuerySnapshot<Map<String, dynamic>>> todosSnapshotStream =
   //       todoCollectionRef.snapshots();
-  //   return todosSnapshotStream.map((snapshot) {
+
+  //   final Stream<List<Todo>> todosStream = todosSnapshotStream.map((snapshot) {
   //     final List<QueryDocumentSnapshot<Map<String, dynamic>>> todoDocuments =
   //         snapshot.docs;
   //     final List<Todo> retrievedTodos = [];
-  //     for (final todoDocument in todoDocuments) {
+  //     for (final QueryDocumentSnapshot<Map<String, dynamic>> todoDocument
+  //         in todoDocuments) {
   //       retrievedTodos.add(Todo.fromFirestore(todoDocument));
   //     }
-  //
+
   //     return retrievedTodos;
   //   });
+
+  //   return todosStream;
   // }
+
+  // Future<Todo> getTodo(String todoId) async {
+  //   final todoCollection = _firestore.collection('todos');
+  //   final todoDocument = await todoCollection.doc(todoId).get();
+  //   final todo = Todo.fromFirestore(todoDocument);
+
+  //   return todo;
+  // }
+
+  Future<Todo> getTodo(String todoId) async => Todo.fromFirestore(
+      await _firestore.collection('todos').doc(todoId).get());
 
   @override
   Future<void> setTodoCompletion(Todo todoToChange, bool isDone) async {
-    // Todo von Firestore updaten
-    final CollectionReference<Map<String, dynamic>> todoCollectionRef =
-        _firestore.collection('todos');
+    final todoCollectionRef = _firestore.collection('todos');
     await todoCollectionRef.doc(todoToChange.id).update({
       'isDone': isDone,
     });
@@ -58,26 +157,38 @@ class FirebaseTodoRepository implements TodoRepository {
 
   @override
   void resetTodos() {
-    final CollectionReference<Map<String, dynamic>> todoCollectionRef =
-        _firestore.collection('todos');
+    final todoCollectionRef = _firestore.collection('todos');
     todoCollectionRef.get().then((snapshot) {
       for (final doc in snapshot.docs) {
         doc.reference.delete();
       }
     }).then((_) {
       for (final todo in _defaultTodos) {
-        todoCollectionRef.add({
-          'topic': todo.topic,
-          'isDone': todo.isDone,
-        });
+        todoCollectionRef.add(todo.toMap());
+        // todoCollectionRef.add({
+        //   'topic': todo.topic,
+        //   'isDone': todo.isDone,
+        // });
       }
     });
   }
 
+  void setTodosWithId() {
+    final todoCollectionRef = _firestore.collection('todos');
+
+    for (int i = 0; i < _defaultTodos.length; i++) {
+      // Beispiel:
+      // todoCollectionRef.doc("0").set({
+      //   "topic": "Gassi gehen",
+      //   "isDone": false,
+      // });
+      todoCollectionRef.doc(i.toString()).set(_defaultTodos[i].toMap());
+    }
+  }
+
   @override
   void deleteTodo(Todo todo) {
-    final CollectionReference<Map<String, dynamic>> todoCollectionRef =
-        _firestore.collection('todos');
+    final todoCollectionRef = _firestore.collection('todos');
     todoCollectionRef.doc(todo.id).delete();
   }
 }
--- lib/domain/todo.dart
+++ lib/domain/todo.dart
@@ -4,6 +4,7 @@ class Todo {
   Todo({required this.id, required this.topic, this.isDone = false});
 
   /// Damit kann ein Todo eindeutig identifiziert werden.
+  /// Wird der ID aus Firestore zugewiesen.
   final String id;
 
   /// Was erledigt werden soll.
@@ -14,11 +15,21 @@ class Todo {
 
   /// Takes a data structure from firestore and creates a Todo object.
   factory Todo.fromFirestore(DocumentSnapshot doc) {
+    if (!doc.exists) throw ArgumentError("Document has no content");
+
     final Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return Todo(
       id: doc.id,
       topic: data['topic'] as String,
       isDone: data['isDone'] as bool,
     );
+  }
+
+  /// Gibt NICHT die ID mit zurück. Diese soll automatisch generiert werden.
+  Map<String, dynamic> toMap() {
+    return {
+      "topic": topic,
+      "isDone": isDone,
+    };
   }
 }
